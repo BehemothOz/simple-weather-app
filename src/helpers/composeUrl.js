9e3ae63ea6d291dbbf62ffffaf6beff371171da7
behemothoz <behemothoz@yandex.ru>
2018-08-27 14:25:06 +0300

    fix

--- src/helpers/composeUrl.js
+++ src/helpers/composeUrl.js
@@ -1,5 +1,5 @@
 export const composeUrl = (searchType, param) => {
   const type = searchType === `name` ? `q` : `id`;
-  const url = `http://api.openweathermap.org/data/2.5/weather?${type}=${param}&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?${type}=${param}&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=metric`;
   return url;
 };
