fd5344a5ae89c6ec348373e4609f983766e609b3
behemothoz <behemothoz@yandex.ru>
2018-08-28 15:14:49 +0300

    import className for ButtonCity component

--- src/components/ButtonCity/index.js
+++ src/components/ButtonCity/index.js
@@ -1,14 +1,19 @@
 import React from "react";
+import classNames from "classnames";
 
 import "./style.css";
 
 export default function ButtonCity(props) {
   const { id, name } = props.city;
+  const { currentCityId } = props;
+
+  const styleButton = classNames({
+    button: true,
+    active: currentCityId === id
+  });
+
   return (
-    <button
-      className={`button ${props.currentCityId === id ? "active" : ""}`}
-      onClick={props.getWeatherForCity(id)}
-    >
+    <button className={styleButton} onClick={props.getWeatherForCity(id)}>
       {name}
     </button>
   );
